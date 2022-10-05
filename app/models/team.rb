0522606cb9631a9cbd85c6542e2c883a2fde1be7
franciscognclvs <fgp1991@gmail.com>
2022-10-04 21:00:31 -0300

    inclui validações no model

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,9 @@
 
 class Team < ApplicationRecord
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :description, presence: true
+
+  validates :name, uniqueness: true
 end
