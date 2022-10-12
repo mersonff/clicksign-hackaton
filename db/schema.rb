e628e7abafbe605d5f53515cd9f472885bee363d
wandeson paiva <wandeson.jpaiva@gmail.com>
2022-10-11 21:17:13 -0300

    Cria tabela de stages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_230211) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_225937) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,13 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_230211) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "teams", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "blacklisted_tokens", force: :cascade do |t|
     t.string "token"
     t.integer "user_id", null: false
@@ -65,6 +58,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_230211) do
     t.index ["user_id"], name: "index_refresh_tokens_on_user_id"
   end
 
+  create_table "stages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -74,7 +80,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_230211) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
