e0838bbd0df4a101d388a0971cce332eb3824a9c
wandeson paiva <wandeson.jpaiva@gmail.com>
2022-10-29 11:28:30 -0300

    Adiciona actions de crud faltante

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,9 +1,29 @@
 class StagesController < ApplicationController
+  before_action :set_stage, only: %i[show update destroy]
+
+  def index
+    @stages = Stage.all
+
+    render json: @stages
+  end
+
+  def show
+    render json: @stage
+  end
+
   def create
     @stage = Stage.new(stages_params)
 
     if @stage.save
       head :created
+    else
+      render json: @stage.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @stage.update(stages_params)
+      head :ok
     else
       render json: @stage.errors, status: :unprocessable_entity
     end
@@ -23,5 +43,9 @@ class StagesController < ApplicationController
 
   def stages_params
     params.require(:stage).permit(:name)
+  end
+
+  def set_stage
+    @stage = Stage.find(params[:id])
   end
 end
