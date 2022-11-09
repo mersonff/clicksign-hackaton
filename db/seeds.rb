95ff1b2a650b1217e3d21202485149bb9cce684c
Hugo-NF <hugonfonseca@hotmail.com>
2022-11-08 21:54:48 -0300

    Adiciona vegeta ao projeto

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,8 @@
 
 # Creates users
 User.create(name: 'John Doe', email: 'john@clicksign.com', password: 'foobar123', token_issued_at: Time.current)
+
+Stage.create(name: 'Fase de Grupos (Rodada 1)')
+
+Team.create(name: 'Brasil', description: 'Seleção Canarinho')
+Team.create(name: 'Argentina', description: 'La mano de Dios')
