08461d625e19bc8e1c4b1872089200ca8921db63
wandeson paiva <wandeson.jpaiva@gmail.com>
2022-11-21 21:57:16 -0300

    Ajusta teste do job de update de matchs

--- spec/jobs/update_matches_job_spec.rb
+++ spec/jobs/update_matches_job_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe UpdateMatchesJob do
   include ActiveJob::TestHelper
 
   describe '#perform_now' do
-    let(:matches) { create_list(:match, 10, start_at: DateTime.current, finished_at: nil) }
+    let(:matches) { create_list(:match, 10) }
 
     before do
       clear_enqueued_jobs
@@ -15,12 +15,10 @@ RSpec.describe UpdateMatchesJob do
     context 'when called' do
       before do
         matches
-        travel(100.minutes)
         described_class.perform_now
       end
 
       it :aggregate_failures do
-        expect(matches.first.reload.finished_at).not_to be_nil
         expect(described_class).to have_been_enqueued
       end
     end
